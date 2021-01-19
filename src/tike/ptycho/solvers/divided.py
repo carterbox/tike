3c9a3b04cb87d2c09aed75603062ae05fc123e3d
Daniel Ching <carterbox@users.noreply.github.com>
2021-01-18 19:07:59 -0600

    REF: Refactor ptycho.lstsq_grad to avoid division
    Replace division normalization with weighted sums for common directions
    and updates.

--- src/tike/ptycho/solvers/divided.py
+++ src/tike/ptycho/solvers/divided.py
@@ -2,7 +2,7 @@ import logging
 
 import cupy as cp
 
-from tike.linalg import lstsq, projection
+from tike.linalg import lstsq, projection, norm
 
 from ..position import update_positions_pd
 from ..probe import orthogonalize_eig, get_unique, update_coherent_probe
@@ -82,93 +82,87 @@ def lstsq_grad(
         logger.info('%10s cost is %+12.5e', 'farplane', cost)
         # TODO: Only compute cost every 20 iterations or on a log sampling?
 
-    # Use χ (chi) to solve the nearplane problem. We use least-squares to
+    # Use χ (diff) to solve the nearplane problem. We use least-squares to
     # find the update of all the search directions: object, probe,
     # positions, etc that causes the nearplane wavefront to match the one
     # we just found by solving the phase problem.
     farplane = op.propagation.adj(farplane, overwrite=True)
-    chi = [
-        farplane[..., m:m + 1, :, :] - nearplane[..., m:m + 1, :, :]
-        for m in range(probe.shape[-3])
-    ]
+
+    # -------------------------------------------------------------------------
 
     # To solve the least-squares optimal step problem we flatten the last
     # two dimensions of the nearplanes and convert from complex to float
-    lstsq_shape = (*nearplane.shape[:-3], 1,
-                   nearplane.shape[-2] * nearplane.shape[-1] * 2)
+    lstsq_shape = (*nearplane.shape[:-3], 1, (pad - end) * (pad - end) * 2)
 
     for m in range(probe.shape[-3]):
-        chi_ = chi[m]
+
+        nearplane = farplane[..., m:m + 1, pad:end, pad:end]
+
         probe_ = common_probe[..., m:m + 1, :, :]
         uprobe_ = unique_probe[..., m:m + 1, :, :]
 
-        logger.info('%10s cost is %+12.5e', 'nearplane',
-                    cp.linalg.norm(cp.ravel(chi_)))
+        patches = cp.zeros(nearplane.shape, dtype='complex64')
+        patches = op.diffraction._patch(
+            patches=patches,
+            psi=psi,
+            scan=scan_,
+            fwd=True,
+        )
+
+        diff = nearplane - uprobe_ * patches
+
+        logger.info('%10s cost is %+12.5e', 'nearplane', norm(diff))
 
         updates = []
 
         if recover_psi:
-            # FIXME: Implement conjugate gradient
-            grad_psi = chi_.copy()
-            grad_psi[..., pad:end, pad:end] *= cp.conj(uprobe_)
+            grad_psi = cp.conj(uprobe_) * diff
 
-            probe_intensity = cp.ones(
-                (*scan_.shape[:2], 1, 1, 1, 1),
-                dtype='complex64',
-            ) * cp.square(cp.abs(uprobe_))
+            # Weight object updates by total probe intensity in each view
+            total_illumination = uprobe_ * uprobe_.conj()
+            total_illumination /= norm(
+                total_illumination,
+                axis=(-5, -4, -3, -2, -1),
+                keepdims=True,
+            )
+            assert total_illumination.dtype == 'complex64'
 
-            norm_probe = op.diffraction._patch(
-                patches=probe_intensity,
-                psi=cp.ones_like(psi),
+            # (25b) Common object gradient
+            common_grad_psi = op.diffraction._patch(
+                patches=grad_psi * total_illumination,
+                psi=cp.zeros(psi.shape, dtype='complex64'),
                 scan=scan_,
                 fwd=False,
-            ) + 1e-6
-
-            # FIXME: What to do when elements of this norm are zero?
-            dir_psi = op.diffraction._patch(
-                patches=grad_psi,
-                psi=cp.zeros_like(psi),
-                scan=scan_,
-                fwd=False,
-            ) / norm_probe
+            )
 
             dOP = op.diffraction._patch(
-                patches=cp.zeros((*scan_.shape[:2], *data_.shape[-2:]),
-                                 dtype='complex64'),
-                psi=dir_psi,
+                patches=cp.zeros(patches.shape, dtype='complex64'),
+                psi=common_grad_psi,
                 scan=scan_,
                 fwd=True,
-            )
-            dOP = dOP.reshape(op.ntheta, scan_.shape[-2], 1, 1,
-                              op.detector_shape, op.detector_shape)
-            dOP[..., pad:end, pad:end] *= uprobe_
-
-            updates.append(dOP)
+            ) * uprobe_
+            updates.append(dOP.view('float32').reshape(lstsq_shape))
 
         if recover_probe:
-            patches = op.diffraction._patch(
-                patches=cp.zeros(data_.shape, dtype='complex64'),
-                psi=psi,
-                scan=scan_,
-                fwd=True,
+            grad_probe = xp.conj(patches) * diff
+
+            # Weight probes in areas of higher transmission as more important
+            total_transmission = patches * patches.conj()
+            total_transmission /= norm(
+                total_transmission,
+                axis=(-5, -4, -3, -2, -1),
+                keepdims=True,
             )
-            patches = patches.reshape(op.ntheta, scan_.shape[-2], 1, 1,
-                                      op.detector_shape, op.detector_shape)
 
-            # (24a) steepest descent update
-            grad_probe = (chi_ * xp.conj(patches))[..., pad:end, pad:end]
+            # (25a) Common probe gradient
+            common_grad_probe = cp.sum(
+                grad_probe * total_transmission,
+                axis=-5,
+                keepdims=True,
+            )
 
-            psi_intensity = cp.square(cp.abs(patches[..., pad:end, pad:end]))
-            norm_psi = cp.sum(psi_intensity, axis=1, keepdims=True) + 1e-6
-
-            # (25a) common update direction
-            dir_probe = cp.sum(grad_probe, axis=1, keepdims=True) / norm_psi
-
-            # ΔPO from (21)
-            dPO = patches.copy()
-            dPO[..., pad:end, pad:end] *= dir_probe
-
-            updates.append(dPO)
+            dPO = common_grad_probe * patches
+            updates.append(dPO.view('float32').reshape(lstsq_shape))
 
         if recover_probe and coherent_probe is not None:
             logger.info('Updating coherent probes')
@@ -186,11 +180,9 @@ def lstsq_grad(
                     )
 
                 # Determine new weights for the updated coherent probe
-                phi = patches.copy()
-                phi[..., pad:end, pad:end] *= coherent_probe[..., c:c + 1,
-                                                             m:m + 1, :, :]
+                phi = patches * coherent_probe[..., c:c + 1, m:m + 1, :, :]
                 n = cp.mean(
-                    cp.real(chi_ * phi.conj()),
+                    cp.real(diff * phi.conj()),
                     axis=(-1, -2),
                     keepdims=True,
                 )
@@ -218,58 +210,48 @@ def lstsq_grad(
 
         # Use least-squares to find the optimal step sizes simultaneously
         # for all search directions.
-        λ = 0.5
-        if len(updates) == 1:  # (23)
-            dX = updates[0]
-            A = cp.sum(dX * dX.conj(), axis=(-2, -1))
-            A += cp.mean(A) * λ
-            b = cp.sum(cp.real(chi_ * dX.conj()), axis=(-2, -1))
-            steps = (b / A)[..., None]
-        elif len(updates) == 2:  # (22)
-            A = cp.empty((*dOP.shape[:-2], 2, 2), dtype='complex64')
-            A[..., 0, 0] = cp.sum(dOP * dOP.conj(), axis=(-2, -1))
-            A[..., 0, 0] += cp.mean(A[..., 0, 0]) * λ
-            A[..., 0, 1] = cp.sum(dOP * dPO.conj(), axis=(-2, -1))
-            A[..., 1, 0] = A[..., 0, 1].conj()
-            A[..., 1, 1] = cp.sum(dPO * dPO.conj(), axis=(-2, -1))
-            A[..., 1, 1] += cp.mean(A[..., 0, 0]) * λ
-
-            b = cp.empty((*dOP.shape[:-2], 2), dtype='complex64')
-            b[..., 0] = cp.sum(cp.real(chi_ * dOP.conj()), axis=(-2, -1))
-            b[..., 1] = cp.sum(cp.real(chi_ * dPO.conj()), axis=(-2, -1))
-            steps = lstsq(A, b)
+        steps = lstsq(
+            a=cp.stack(updates, axis=-1),
+            b=diff.view('float32').reshape(lstsq_shape),
+        )
 
         num_steps = 0
-        d = 0
 
         # Update each direction
         if recover_psi:
             step = steps[..., num_steps, None, None]
             num_steps += 1
 
+            # (27b) Object update
             weighted_step = op.diffraction._patch(
-                patches=step * probe_intensity,
-                psi=cp.zeros_like(psi),
+                patches=step * total_illumination,
+                psi=cp.zeros(psi.shape, dtype='complex64'),
                 scan=scan_,
                 fwd=False,
             )
 
-            psi += dir_psi * weighted_step / norm_probe
-            d += step * dOP
+            psi += weighted_step * common_grad_psi
 
         if recover_probe:
             step = steps[..., num_steps, None, None]
             num_steps += 1
 
-            # Common probe update (27a)
-            weighted_step = cp.sum(step * psi_intensity, axis=1, keepdims=True)
-            probe_ += dir_probe * weighted_step / norm_psi
-
-            d += step * dPO
+            # (27a) Probe update
+            probe_ += common_grad_probe * cp.sum(
+                step * total_transmission,
+                axis=-5,
+                keepdims=True,
+            )
 
         if __debug__:
+            patches = op.diffraction._patch(
+                patches=cp.zeros(nearplane.shape, dtype='complex64'),
+                psi=psi,
+                scan=scan_,
+                fwd=True,
+            )
             logger.info('%10s cost is %+12.5e', 'nearplane',
-                        cp.linalg.norm(cp.ravel(chi_ - d)))
+                        norm(probe_ * patches - nearplane))
 
     if recover_probe and probe.shape[-3] > 1:
         probe = orthogonalize_eig(probe)
