2536600d655874ae98078cdc16022c47f9728523
Daniel Ching <carterbox@users.noreply.github.com>
2021-01-27 13:18:16 -0600

    NEW: Add batch_indices function

--- src/tike/opt.py
+++ src/tike/opt.py
@@ -82,6 +82,17 @@ def adam(d, v=None, m=None, vdecay=0.9, mdecay=0.999, eps=1e-6):
     return m_ / (v_ + eps), v, m
 
 
+def batch_indicies(n, m=None, use_random=False):
+    """Return list of indices [0...n) as groups of at most m indices.
+
+    >>> batch_indicies(10, 4)
+    [array([2, 4, 7, 3]), array([1, 8, 9]), array([6, 5, 0])]
+    """
+    m = n if m is None else m
+    i = randomizer.permutation(n) if use_random else np.arange(n)
+    return np.array_split(i, (n + m - 1) // m)
+
+
 def line_search(
     f,
     x,
